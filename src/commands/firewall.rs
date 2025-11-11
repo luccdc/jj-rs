d3b8536bcee4450ebc97eef4cbf8c89a8b353d73
Andrew Rioux <rioux.d.andrew@gmail.com>
2025-11-11 00:10:10 -0500

    fix: check writeln! result

--- src/commands/firewall.rs
+++ src/commands/firewall.rs
@@ -112,7 +112,7 @@ impl QuickSetup {
         writeln!(
             ob,
             "        icmp type {{ echo-request, echo-reply }} ct state new accept"
-        );
+        )?;
         writeln!(ob, "        ct state established,related accept")?;
         writeln!(ob, r#"        log prefix "inbound-drop: " drop"#)?;
         writeln!(ob, "    }}\n")?;
