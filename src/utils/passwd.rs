2ba930311727712e2b623301fb792f43688718c5
Andrew Rioux <rioux.d.andrew@gmail.com>
2026-01-20 16:18:36 -0500

    feat: add the ability to query group

--- src/utils/passwd.rs
+++ src/utils/passwd.rs
@@ -16,6 +16,15 @@ pub struct Passwd {
     pub shell: String,
 }
 
+/// Matches the structure of man 5 group
+#[allow(dead_code)]
+pub struct Group {
+    pub name: String,
+    pub password: String,
+    pub gid: u32,
+    pub user_list: Vec<String>,
+}
+
 /// Read passwd database entries
 ///
 /// Allows specifying a user filter. Particularly useful if looking for the database
@@ -83,6 +92,61 @@ pub fn load_users<I: Into<Option<S>>, S: AsRef<str>>(uid: I) -> eyre::Result<Vec
                 gecos,
                 home,
                 shell,
+            })
+        })
+        .collect())
+}
+
+/// Read group database entries
+///
+/// Allows specifying a group filter. Particularly useful if looking for the database
+/// entry for a specific user
+///
+/// ```
+/// # use jj_rs::utils::passwd::load_groups;
+/// # fn test_load_groups() -> eyre::Result<()> {
+/// // load all users
+/// let groups = load_groups::<_, &str>(None)?;
+/// for group in groups {
+///     println!("Found group {}: {}", group.gid, &group.name);
+/// }
+/// # Ok(())
+/// # }
+/// # test_load_groups().expect("could not load root");
+/// ```
+pub fn load_groups<I: Into<Option<S>>, S: AsRef<str>>(uid: I) -> eyre::Result<Vec<Group>> {
+    // getent group works better for domain joined systems and systems with weird
+    // /etc/nsswitch.conf, but fall back to directly reading from /etc/group
+    let cmd = match uid.into() {
+        Some(a) => {
+            format!("getent group {}", a.as_ref())
+        }
+        None => "getent group".to_string(),
+    };
+
+    let passwd = match qx(&cmd) {
+        Ok((e, s)) if e.success() && !s.is_empty() => s.trim().to_string(),
+        _ => String::from_utf8_lossy(&std::fs::read("/etc/group")?).to_string(),
+    };
+
+    Ok(passwd
+        .split('\n')
+        .filter_map(|row| -> Option<Group> {
+            let mut options = row.split(':');
+            let name = options.next()?.to_string();
+            let password = options.next()?.to_string();
+            let gid = options.next()?.parse().ok()?;
+            let user_list = options
+                .next()?
+                .split(',')
+                .map(str::to_string)
+                .collect::<Vec<_>>();
+
+            Some(Group {
+                name,
+                password,
+                gid,
+                user_list,
             })
         })
         .collect())
