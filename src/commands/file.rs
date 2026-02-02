eb771c9a99b4916aebb1898a2a051dbff301949e
Judah Sotomayor <jisotomayor@liberty.edu>
2026-02-01 21:20:23 -0500

    feat: Convert file command to use walkdir.
    This change completely eliminates the old get_file_paths function in
    favor of WalkDir.
    
    Notes:
    - WalkDir's depth is currently set to either 10 or nothing, depending on
    whether -r is passed. This could easily be parameterized, perhaps r
    should optionally provide a number.
    - Colors, symlinks, and other open issues remain at large.

--- src/commands/file.rs
+++ src/commands/file.rs
@@ -1,9 +1,13 @@
+use walkdir::WalkDir;
 use std::{
+    fs,
     io::{Write, BufReader, BufRead, Read},
-    path::PathBuf,
-    collections::HashSet,
+    path::{Path, PathBuf },
+    collections::{ HashMap, HashSet },
 };
 
+use tracing::warn;
+use eyre::{ Result};
 use sha2::{Sha256, Digest};
 
 use chrono::Local;
@@ -15,15 +19,15 @@ use clap::{Parser, Subcommand, Args};
 pub struct CommonArgs {
     /// Defines paths to evaluate
     #[arg(short = 'f', long = "files", num_args = 1.., value_delimiter = ',')]
-    path_arg: Option<Vec<PathBuf>>,
+    path: Option<Vec<PathBuf>>,
 
     /// Sets location of hashfile
     #[arg(short = 'l', long = "location", default_value = "./jj_hashes.txt")]
-    hashfile_arg: PathBuf,
+    hashfile: PathBuf,
 
     /// Search directories recursively
     #[arg(short = 'r', long = "recursive")]
-    recursive_arg: bool,
+    recursive: bool,
 
 }
 
@@ -63,24 +67,28 @@ struct SaveHashes {
 
 impl SaveHashes {
     fn execute(self) -> eyre::Result<()> {
-        let mut ob: Box<dyn Write> = Box::new(std::fs::File::create(&self.common.hashfile_arg)?);
+        let mut ob: Box<dyn Write> = Box::new(fs::File::create(&self.common.hashfile)?);
 
-        let paths: Vec<PathBuf> = match &self.common.path_arg {
+        let paths: Vec<PathBuf> = match &self.common.path {
             Some(v) => v.clone(),
             None => vec![PathBuf::from(".")],
         };
 
-        let tuple: (Vec<PathBuf>, Vec<PathBuf>) = get_file_paths(&self.common, paths);
-        
-        for dir in tuple.1 {
-            let dirpath = dir.display();
-            writeln!(ob, "D {dirpath}")?;
-        }
-        for file in tuple.0 {
-            let filepath = file.display();
-            let hash = sha256_file(&file)?;
-            let time = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
-            writeln!(ob, "F {filepath} {hash} {time}")?;
+        for path in paths {
+            for entry in WalkDir::new(path).max_depth(if self.common.recursive { 10 } else { 1 }) {
+                let e = entry?;
+                if e.path().is_file() {
+
+                    writeln!(ob, "F {} {} {}",
+                             e.path().display(),
+                             sha256_file(e.path())?,
+                             Local::now().format("%Y-%m-%d %H:%M:%S"),
+                    )?;
+
+                } else if e.path().is_dir() {
+                    writeln!(ob, "D {}", e.path().display())?;
+                }
+            }
         }
 
         Ok(())
@@ -94,136 +102,94 @@ struct VerifyHashes {
 
     /// Only output changed files
     #[arg(short = 's', long = "short")]
-    short_arg: bool,
+    short: bool,
 
     /// Verify all files in hash file
     #[arg(short = 'a', long = "all")]
-    all_arg: bool,
+    all: bool,
 }
+
 
 impl VerifyHashes {
     fn execute(self) -> eyre::Result<()> {
-        let hashfile = std::fs::File::open(self.common.hashfile_arg.clone())?;
-        let hashfile_reader = BufReader::new(hashfile);
+        let hashfile_reader = BufReader::new(
+            fs::File::open(self.common.hashfile.clone())?
+        );
 
-        let mut tracked_dirs: Vec<PathBuf> = Vec::new();
-        let mut tracked_files: Vec<(PathBuf, String)> = Vec::new();
+        let mut tracked_dirs = HashSet::new();
+        let mut tracked_files = HashMap::new();
 
         for line_result in hashfile_reader.lines() {
             let line: String = line_result?;
-            let mut parts = line.split_whitespace();
-            let pathtype = match parts.next() {
-                Some(t) => t.to_string(),
-                None => continue,
-            };
-            let path = match parts.next() {
-                Some(p) => PathBuf::from(p),
-                None => continue,
-            };
-            
-            if pathtype == "D" {
-                tracked_dirs.push(path);
-            }
-            else if pathtype == "F" {
-                let old_hash = match parts.next() {
-                    Some(h) => h.to_string(),
-                    None => continue,
-                };
-                tracked_files.push((path, old_hash));
-            }
-        }
-    
-        let paths: Vec<PathBuf> = match &self.common.path_arg {
-            Some(v) => v.clone(),
-            None => tracked_dirs,
-        };
 
-        let tuple: (Vec<PathBuf>, Vec<PathBuf>) = get_file_paths(&self.common, paths);
-        let current_files = tuple.0;
-
-        let hashset_current: HashSet<&PathBuf> = current_files.iter().collect();
-        let hashset_old: HashSet<&PathBuf> = tracked_files.iter().map(|(p,_)| p).collect();
-        let intersection: Vec<(PathBuf,String)> = tracked_files
-            .iter()
-            .filter(|(path, _)| hashset_current.contains(path))
-            .cloned()
-            .collect();
-        let only_old: Vec<(PathBuf,String)> = tracked_files //This needs to be checked against the supplied dirs
-            .iter()
-            .filter(|(path, _)| !hashset_current.contains(path))
-            .cloned()
-            .collect();
-        let only_current: Vec<PathBuf> = current_files
-            .iter()
-            .filter(|path| !hashset_old.contains(path))
-            .cloned()
-            .collect();
-
-        for (path, old_hash) in intersection {
-            let path_str = path.display();
-            let new_hash = sha256_file(&path)?;
-            if new_hash == old_hash {
-                if !self.short_arg {
-                    println!("[1] {path_str}");
+            let mut split = line.split_whitespace();
+            if let (Some( pathtype ), Some(path)) =  (split.next(), split.next()) {
+                match pathtype {
+                    "D" => { tracked_dirs.insert(PathBuf::from(path));},
+                    "F" => { tracked_files.insert(PathBuf::from(path), split.next().expect("No hash!!").to_string()); },
+                    _ => warn!("Unrecognized filetype!"),
                 }
             }
-            else {
-                println!("[0] {path_str}");
+        }
+
+        let paths: Vec<PathBuf> = match &self.common.path {
+            Some(v) => v.clone(),
+            None => tracked_dirs.clone().into_iter().collect::<Vec<_>>(),
+        };
+
+        for path in paths {
+            for entry in WalkDir::new(path).max_depth(if self.common.recursive { 10 } else { 1 }) {
+                let e = entry?;
+                let disp = e.path().display();
+                if e.path().is_file() {
+                    // Check if present in tracked_paths, otherwise it's new.
+                    if let Some(old_hash) = tracked_files.remove(e.path()) {
+                        // Ok, it's present. Is it valid? 
+                        let new_hash = sha256_file(e.path())?;
+                        if new_hash == old_hash {
+                            //Great! It's valid!
+                            if !self.short {
+                                println!("[1] {disp}");
+                            }
+                        }
+                        else {
+                            // Invalid!
+                            println!("[0] {disp}");
+                        }
+                    } else {
+                        // New!
+                        println!("[+] {disp}");
+                    }
+
+                } else if e.path().is_dir() {
+                    if tracked_dirs.remove(e.path()) {
+                        // Good dir
+                        if !self.short {
+                            println!("[1] {disp}/");
+                        }
+                    } else {
+                        // New dir
+                        println!("[+] {disp}/");
+                    }
+                }
             }
         }
-        for (path, _) in only_old {
-            let path_str = path.display();
-            println!("[-] {path_str}");
+
+        // Missing dir/file
+        for path in tracked_files.keys() {
+            println!("[-] {}", path.display());
         }
-        for path in only_current {
-            let path_str = path.display();
-            println!("[+] {path_str}");
+
+        for path in tracked_dirs {
+            println!("[-] {}/", path.display());
         }
 
         Ok(())
     }
 }
 
-fn get_file_paths(args: &CommonArgs, paths: Vec<PathBuf>) -> (Vec<PathBuf>, Vec<PathBuf>) {
-    let mut files: Vec<PathBuf> = Vec::new();
-    let mut dirs: Vec<PathBuf> = Vec::new();
-    let mut more_dirs: Vec<PathBuf> = Vec::new();
-
-    let abs_paths: Vec<PathBuf> = paths        
-        .into_iter()
-        .filter_map(|p| std::fs::canonicalize(&p).ok())
-        .collect();
-
-    for path in abs_paths{
-        if path.is_file() {
-            files.push(path);
-        } else if path.is_dir() {
-            dirs.push(path.clone());
-            if let Ok(entries) = std::fs::read_dir(path) {
-                for entry in entries.filter_map(Result::ok) {
-                    let p = entry.path();
-                    if p.is_file() {
-                        files.push(p);
-                    }
-                    else if args.recursive_arg && p.is_dir() {
-                        more_dirs.push(p);
-                    }
-                }
-            }
-        }
-    }
-
-    if args.recursive_arg && more_dirs.len() > 0 {
-        let mut new_tuple: (Vec<PathBuf>, Vec<PathBuf>) = get_file_paths(args, more_dirs);
-        files.append(&mut new_tuple.0);
-        dirs.append(&mut new_tuple.1);
-    }
-
-    (files, dirs)
-}
-
-fn sha256_file(path: &std::path::PathBuf) -> std::io::Result<String> {
-    let file = std::fs::File::open(path)?;
+fn sha256_file(path: &Path) -> Result<String> {
+    let file =  fs::File::open(path)?;
     let mut reader = BufReader::new(file);
     let mut hasher = Sha256::new();
     let mut buffer = [0u8; 8192];
@@ -236,5 +202,5 @@ fn sha256_file(path: &std::path::PathBuf) -> std::io::Result<String> {
     }
 
     let result = hasher.finalize();
-    Ok(format!("{:x}", result))
+    Ok(format!("{result:x}"))
 }
