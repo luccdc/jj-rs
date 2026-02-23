ae29e809b728550f82c258ec12dc67040b6c2aae
Andrew Rioux <rioux.d.andrew@gmail.com>
2026-02-23 18:33:40 -0500

    feat: add UI for adding SMTP checks
    Currently has a bug where only IP addresses are accepted for host names

--- src/utils/clap.rs
+++ src/utils/clap.rs
@@ -1,7 +1,7 @@
 use std::net::IpAddr;
 use std::str::FromStr;
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone)]
 pub enum Host {
     Ip(IpAddr),
     Domain(String),
@@ -12,6 +12,60 @@ impl std::fmt::Display for Host {
         match self {
             Host::Ip(ip) => ip.fmt(f),
             Host::Domain(host) => host.fmt(f),
+        }
+    }
+}
+
+impl serde::Serialize for Host {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        match &self {
+            Host::Ip(i) => serializer.serialize_str(&format!("{}", i)),
+            Host::Domain(s) => serializer.serialize_str(s),
+        }
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for Host {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        struct HostVisitor;
+
+        impl serde::de::Visitor<'_> for HostVisitor {
+            type Value = Host;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("Host")
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                Ok(v.into())
+            }
+
+            fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                self.visit_str(&v)
+            }
+        }
+
+        deserializer.deserialize_str(HostVisitor)
+    }
+}
+
+impl From<&str> for Host {
+    fn from(v: &str) -> Self {
+        match IpAddr::from_str(v) {
+            Ok(ip) => Host::Ip(ip),
+            _ => Host::Domain(v.to_owned()),
         }
     }
 }
