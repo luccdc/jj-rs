164a8b33596219d70937383b6ab1d5fe465db658
Andrew Rioux <rioux.d.andrew@gmail.com>
2026-03-24 22:03:01 -0400

    fix: add logging and populate TLS cache
    "Freshclam.exe fails due to TLS verification"
    Solution?
    "invoke-webrequest in powershell at the same URL first"

--- src/commands/clamav_windows.rs
+++ src/commands/clamav_windows.rs
@@ -17,23 +17,34 @@ pub struct ClamAv {
 
 impl super::Command for ClamAv {
     fn execute(self) -> eyre::Result<()> {
+        println!("--- Installing ClamAV...");
         install_clamav(self.clamav_url)
     }
 }
 
 pub fn install_clamav(clamav_url: String) -> eyre::Result<()> {
-    let working_dir = std::env::temp_dir();
-    let target_path = working_dir.join("clamav.zip");
-    download_file(&clamav_url, &target_path)?;
+    if !std::path::PathBuf::from(r"C\Program Files\ClamAV").exists() {
+        println!("--- Downloading ClamAV...");
+        let working_dir = std::env::temp_dir();
+        let target_path = working_dir.join("clamav.zip");
+        download_file(&clamav_url, &target_path)?;
 
-    let archive =
-        std::io::BufReader::new(std::fs::OpenOptions::new().read(true).open(target_path)?);
-    let mut archive = zip::read::ZipArchive::new(archive)?;
+        println!("Downloaded ClamAV. Extracting...");
+        let archive =
+            std::io::BufReader::new(std::fs::OpenOptions::new().read(true).open(target_path)?);
+        let mut archive = zip::read::ZipArchive::new(archive)?;
 
-    archive.extract_unwrapped_root_dir(
-        r"C:\Program Files\ClamAV",
-        zip::read::root_dir_common_filter,
-    )?;
+        archive.extract_unwrapped_root_dir(
+            r"C:\Program Files\ClamAV",
+            zip::read::root_dir_common_filter,
+        )?;
+
+        println!("--- Extraction complete; configuring ClamAV");
+    } else {
+        println!(
+            "--- ClamAV already downloaded and extracted! Run `jj clam-av -O` to override, and make sure that ClamAV isn't running"
+        );
+    }
 
     std::fs::write(r"C:\Program Files\ClamAV\freshclam.conf", FRESHCLAM_CONF)?;
     std::fs::write(r"C:\Program Files\ClamAV\clamd.conf", CLAMAV_CONF)?;
@@ -44,10 +55,24 @@ pub fn install_clamav(clamav_url: String) -> eyre::Result<()> {
         .spawn()?
         .wait()?;
 
+    Command::new("powershell.exe")
+        .args([
+            "-NoProfile",
+            "-ExecutionPolicy",
+            "Bypass",
+            "-Command",
+            "Invoke-WebRequest -Uri 'https://database.clamav.net/daily.cvd' -Method HEAD",
+        ])
+        .output()?;
+
+    println!("--- Downloading ClamAV definitions");
+
     Command::new(r"C:\Program Files\ClamAV\freshclam.exe")
         .current_dir(r"C:\Program Files\ClamAV")
         .spawn()?
         .wait()?;
+
+    println!("--- Starting ClamAV!");
 
     Command::new("net")
         .args(["start", "clamd"])
